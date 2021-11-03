c0dda09032b528d2b6eaea3cb7eb581ecc8ac1a6
NatureNai <93626966+NatureNai@users.noreply.github.com>
2021-11-02 20:36:11 -0400

    Add files via upload

---
+++ preposal repo.py
@@ -0,0 +1,14 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+"""
+Create a LEGO robot face that changes with the images it processes. Currently,
+it is best used with pictures but hopefully I can extend
+its use to facial recognition"""
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+"""N/A """
+## Do you have any questions of your own?
+
+"""not at the moment"""
